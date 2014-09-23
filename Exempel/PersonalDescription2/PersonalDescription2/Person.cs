a49fd5b3036b9a50f9b68a5d2aaa5a84d1087b73
Mats Loock <mats.loock@lnu.se>
2014-09-23 13:14:00 +0200

    Uppdatering.

---
+++ Exempel/PersonalDescription2/PersonalDescription2/Person.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Drawing;
+
+namespace PersonalDescription2
+{
+    class Person
+    {
+        #region Fält
+
+        // Fält.
+        // Fält ska ALLTID vara privata. (En 
+        // tumregel som det är sällsynt att 
+        // man inte följer.)
+        private string _name;
+        private int _shoeSize;
+
+        #endregion
+
+        #region Autoimplementerade egenskaper
+
+        // Någon validering av ögonfärgen behöver inte göras,
+        // varför en autoimplementerad egenskap lämpligen
+        // används istället för ett publikt fält.
+        public Color EyeColor { get; set; }
+
+        #endregion
+
+        #region Egenskaper
+
+        public string Name
+        {
+            get { return this._name; }
+            set
+            {
+                // Namnet måste sättas till en sträng med
+                // tecken, dock inte enbart vita tecken,
+                // d.v.s. mellanslag, tabbar, etc.. Klarar inte
+                // värdet valideringen kastas ett undantag.
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name is required.", "Name");
+                }
+
+                // Trimmar bort inledande och avslutande
+                // vita tecken.
+                this._name = value.Trim();
+            }
+        }
+
+        public int ShoeSize
+        {
+            get { return _shoeSize; }
+            set
+            {
+                // Skostorleken måste sättas till ett värde mellan
+                // 5 och 52. Klarar inte värdet valideringen kastas 
+                // ett undantag.
+                if (value < 5 || value > 52)
+                {
+                    throw new ArgumentOutOfRangeException("ShoeSize", "The shoe size cannot be less than 5 or grater than 52.");
+                }
+                this._shoeSize = value;
+            }
+        }
+
+        #endregion
+
+        #region Konstruktorer
+
+        // "Standardkonstruktor" som tillåter att ett objekt kan instansieras
+        // utan att skicka med några argument.
+        public Person()
+        {
+            // Tom! Det nya objektets fält tilldelas standardvärdena
+            //      för typerna de är av.
+            //      string -> null
+            //      byte   -> 0
+            //      Color  -> 0
+        }
+
+        // Konstruktor som kräver argument för namn, skostorlek och ögonfärg.
+        // Parametrarnas värden kopieras till respektive fält.
+        public Person(string name, int shoeSize, Color eyeColor)
+        {
+            // this refererar till objekt som just skapats och som
+            // här initieras av konstruktorn.
+            Name = name;
+            ShoeSize = shoeSize;
+            EyeColor = eyeColor;
+        }
+
+        #endregion
+
+        #region Metoder
+
+        // Instansmetod.
+        public void ViewData()
+        {
+            string color = null;
+
+            // Översätt fältet _eyeColor till en sträng.
+            if (EyeColor == Color.Blue)
+            {
+                color = "blåa";
+            }
+            else if (EyeColor == Color.Brown)
+            {
+                color = "bruna";
+            }
+            else if (EyeColor == Color.Green)
+            {
+                color = "gröna";
+            }
+            else
+            {
+                color = "okänd färg på sina";
+            }
+
+            // Skriv ut beskrivning av personen.
+            Console.WriteLine("{0} har {1} ögon och {2} i skostorlek.\n", 
+                Name, color, ShoeSize);
+        }
+
+        #endregion
+    }
+}
---
+++ Exempel/PersonalDescription2/PersonalDescription2/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Drawing;
+
+namespace PersonalDescription2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Raderna nedan är bortkommenterade eftersom fälten är privata,
+            // eller inte längre finns, varför de inte kan kommas åt.
+            //Person Person = new Person(); // skapar nytt Person-objekt.
+            //Person._name = "Adam";
+            //Person._shoeSize = 42;
+            //Person._eyeColor = Color.Blue;
+            //Person.ViewData(); // Objektet som Person refererar till exekverar metoden i klassen!
+
+            // Här används publika egenskaper istället för fälten.
+            Person person2 = new Person(); // instansierar nytt objekt av klassen Person.
+            person2.Name = "Bertil";
+            person2.ShoeSize = 42;
+            person2.EyeColor = Color.Brown;
+            person2.ViewData(); // Objektet som person2 refererar till exekverar metoden i klassen!
+
+            // Då en egenskap tilldelas ett värde valideras värdet innan det sparas i det privata fält
+            // som egenskapen är knuten till. Misslyckas valideringen kastas ett undantag.
+            try
+            {
+                Person person3 = new Person();
+                person3.Name = "Ceasar";
+                person3.ShoeSize = 54;  // Orsakar att ett undantag kastas!
+                person3.EyeColor = Color.Green;
+                person3.ViewData(); // Objektet som person3 refererar till exekverar metoden i klassen!
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\nERROR!\n{0}\n", ex.Message);
+            }
+
+            try
+            {
+                Person person4 = new Person("    ", 0, Color.Gray); // Försöker instansiera och initera ett nytt objekt 
+                // av typen Person. Orsakar att ett undantag kastas! 
+                person4.ViewData();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\nERROR!\n{0}\n", ex.Message);
+            }
+
+            try
+            {
+                // Använder standardkonstuktorn.
+                Person person5 = new Person();
+
+                // Tilldelar objektet som person5 refererar till värden
+                // genom att använda de publika egenskaperna.
+                person5.Name = "Erik";
+                person5.ShoeSize = 43;
+                person5.EyeColor = Color.Blue;
+                person5.ViewData();
+
+                // Använder konstuktorn som har tre parametrar för att initiera
+                // objektet person6 refererar till.
+                Person Person6 = new Person("Filip", 41, Color.Brown);
+                Person6.ViewData();
+
+                // Använder standsardkonstuktorn och egenskaper för att
+                // initera objektet person7 refererar till.
+                Person person7 = new Person
+                {
+                    Name = "Gustav",
+                    ShoeSize = 37,
+                    EyeColor = Color.Green
+                };
+                person7.ViewData();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\nERROR!\n{0}\n", ex.Message);
+            }
+        }
+    }
+}
